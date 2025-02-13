822664d350b30660bb8583758cf37d22866f3c2e
Eyal Katz <eyalkatz2403@gmail.com>
2025-02-13 22:03:35 +0200

    Updated view modifier template

--- View Modifier.xctemplate/___FILEBASENAME___.swift
+++ View Modifier.xctemplate/___FILEBASENAME___.swift
@@ -1,3 +1,16 @@
 //___FILEHEADER___
 
-import Foundation
+import SwiftUI
+
+fileprivate struct ___VARIABLE_productName___: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        
+    }
+}
+
+public extension View {
+    func ___VARIABLE_funcName___() -> some View {
+        modifier(___VARIABLE_productName___())
+    }
+}
